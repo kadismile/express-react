cf0356a7d48d17256f8bf5cd8332c37e33266c27
Ibrahim Abubakar Sediq <ibrahim@kadismile.com>
2019-07-21 15:57:04 +0100

    removed the .env

--- routes/posts.js
+++ routes/posts.js
@@ -1,54 +1,17 @@
 var router = require('express').Router();
 const Post = require('../Models/Post')
+const PostController = require('../Controllers/postController')
 
 
-router.get('', async(req, res) => {
-  try {
-    const posts = await Post.find();
-    res.json(posts)
-  }catch(error){
-    res.json({message: error})
-  }
-});
+router.get('',PostController.post_get_all);
 
-router.post('/create', async(req, res)=> {
-  const post = new Post(req.body);
-  try {
-    const savedPost = await post.save();
-    res.send(savedPost)
-  }catch(error){
-    res.status(500).json({status: 'failed', message: error.message })
-  }
+router.post('/create', PostController.post_create);
 
-});
+router.get('/:postId', PostController.post_find_one);
 
-router.get('/:postId', async(req, res) => {
-  try {
-    const post = await Post.findById(req.params.postId);
-    res.json(post)
-  }catch(error){
-    res.status(404).json({status: 'failed', message: error.message })
-  }
-});
+router.patch('/:postId', PostController.post_update);
 
-router.delete('/:postId', async(req, res) => {
-  try {
-    const post = await Post.remove({_id: req.params.postId});
-    res.json(post)
-  }catch(error){
-    res.status(404).json({status: 'failed', message: error.message })
-  }
-});
+router.delete('/:postId',PostController.post_delete);
 
-router.patch('/:postId', async(req, res) => {
-  try {
-    const post = await Post.updateOne({_id: req.params.postId},
-        {$set: req.body}
-      );
-    res.json({message:"Success ", body: post})
-  }catch(error){
-    res.status(404).json({status: 'failed', message: error.message })
-  }
-});
 
 module.exports = router;
