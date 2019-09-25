0070cf50ee98fc709c589b320e572c146bbfb267
Ibrahim Abubakar Sediq <ibrahim@kadismile.com>
2019-09-25 19:14:23 +0100

    added passport and other updates

--- server.js
+++ server.js
@@ -6,20 +6,22 @@ const socketIo = require('socket.io');
 const cors = require('cors');
 const io = socketIo(server);
 const mongoose = require('mongoose');
+const passport = require('passport');
+
 require('dotenv').config();
-
-
+require('./config/passportConfig'); //very important to use passport
 app.use(express.json());
 app.use(cors());
-//conect to tje database
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect to the database
 mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true },
-        ()=> console.log("Connected to the data Base... to express"));
-
-
+  ()=> console.log("Connected to the data Base"));
 
 app.use('/', require('./routes/index'));
 app.use('/customers', require('./routes/customers'));
-app.use('/posts', require('./routes/posts'));
+app.use('/users', require('./routes/users'));
 
 
 io.on('connection', (socket) => {
@@ -29,7 +31,8 @@ io.on('connection', (socket) => {
 });
 
 
-const port = 4000;
+const port = 5000;
 server.listen(port, () => console.log(`server started on port ${port}`));
+
 
 
